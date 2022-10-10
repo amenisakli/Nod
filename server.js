dd914f63af4d871e87684f1fec2b457f238890f4
Alexandre <alexandrerichard45@sfr.fr>
2022-10-10 22:31:44 +0200

    first commit

---
+++ server.js
@@ -0,0 +1,11 @@
+const http = require ('http')
+const express = require('express');
+
+const app = express();
+
+app.set('port',process.env.PORT || 3000);
+const server = http.createServer(app)
+
+server.listen(process.env.PORT || 3000)
+
+module.exports = app;
