2d14ea128a179385c59aa7112530645fdff63e69
Alexandre <alexandrerichard45@sfr.fr>
2022-10-17 12:09:10 +0200

    mise en place de la fonction ajouté sauce sans multer

--- controllers/sauces.js
+++ controllers/sauces.js
@@ -1,6 +1,18 @@
 const Sauce = require('../models/sauces')
 //const jwt = require('jsonwebtoken')
 
+exports.creation = async (req, res) => {
+    const {name ,manufacturer ,description ,mainPepper ,imageUrl ,heat} = req.body
+    const sauce = new Sauce({name ,manufacturer ,description ,mainPepper ,imageUrl ,heat})
+    try{
+        await sauce.save()
+        return res.status(200).json({message : `ajouté !`})
+    }
+    catch(e){
+        return res.status(400).json({e})
+    }
+}
+
 exports.affichageSauces = async (req, res) => {
     const sauces = await Sauce.find()
     if(!sauces){
@@ -13,6 +25,7 @@ exports.affichageSauces = async (req, res) => {
         return res.status(400).json({e})
     }
 }
+
 exports.affichageSauce = async (req, res) => {
     const sauces = await Sauce.findOne({ _id: req.params.id })
     if(!sauces){
